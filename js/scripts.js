2c60557cb5d21c34a96b84eaf11c9a4102f07c9e
Zurekxz <you@example.com>
2021-05-09 20:10:20 -0400

    update index - scripts(fetch)

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
-/*!
-<div class="card shadow-sm h-100">
-    <a href="#!">
-        <img class="shadow-sm card-img-top" src="https://dojiw2m9tvv09.cloudfront.net/11132/product/misterbig3308256.jpg"
-            alt="..." />
-    </a>
-    <div class="card-body shadow-sm">
-        <h6 class="card-title text-center mt-3">Energetica MR BIG</h6>
-        <h5 class="text-center">$ 1.990</h5>
-    </div>
-    <div class="mx-auto my-2 ">
-        <a href="#!">
-            <img  src="/img/outline_add_shopping_cart_black_24dp.png" alt="">
-        </a>
-    </div>
-</div>
-*/
 const mostradorProductos = document.querySelector('#mostrador');
 const carrito = document.querySelector('#carrito');
 const agregarCarrito = document.querySelector('#agregarCarrito');
-const 
+const url = 'http://localhost:3000/api/products';
+addEventListeners();
 
-function displayProducto(producto){
+function addEventListeners(){
+    document.addEventListener('DOMContentLoaded',traerProductos);
+}
+
+function traerProductos(){
+    fetch(`${url}`)
+        .then(res => res.json())
+        .then(data => displayProducto(data)) 
+}
+
+function displayProducto(producto){  
+console.log(producto)
+producto.forEach(product =>{
+    const divCol = document.createElement('div');
     const card = document.createElement("div");
-    const cardbody = document.createElement("div");
-    const cardfooter = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const cardFooter = document.createElement("div");
 
     const linkImagen = document.createElement("a");
     const imagen = document.createElement("img");
@@ -31,14 +28,58 @@ function displayProducto(producto){
     const nombreProducto = document.createElement("h6");
     const precioProducto = document.createElement("h5");
 
-    const LinkAgregarCarrito = document.createElement("a");
-    const AgregarCarrito = document.createElement("img");
+    const linkAgregarCarrito = document.createElement("a");
+    const agregarCarrito = document.createElement("img");
 
-    card.classList.add('card shadow-sm h-100');
-    cardbody.classList.add('card-body shadow-sm');
-    cardfooter.classList.add('mx-auto my-2');
+    //Se agregan clases
+    divCol.classList.add('col-lg-4','col-md-6','mb-4','mt-5')
+    card.classList.add('card','shadow-sm','h-100');
 
-    imagen.classList.add('shadow-sm card-img-top');
+    imagen.classList.add('shadow-sm','card-img-top');
+    
+    cardBody.classList.add('card-body','shadow-sm');
+    nombreProducto.classList.add('card-title','text-center','mt-3');
+    precioProducto.classList.add('text-center')
+    //Se agrega atributos
+    cardFooter.classList.add('mx-auto','my-2');
+    linkAgregarCarrito.setAttribute('href','#!')
+    agregarCarrito.setAttribute('src','/img/outline_add_shopping_cart_black_24dp.png');
 
 
+    if(producto.image !== 1){
+        imagen.setAttribute('src',`${product.image}`);
+    }
+    nombreProducto.textContent = `${product.name}`;
+    precioProducto.textContent = `$ ${product.price}`
+ 
+    mostrador.appendChild(divCol);
+    divCol.appendChild(card);
+    card.appendChild(linkImagen);
+    card.appendChild(cardBody);
+    card.appendChild(cardFooter);
+    linkImagen.appendChild(imagen);
+    cardBody.appendChild(nombreProducto)
+    cardBody.appendChild(precioProducto)
+    cardFooter.appendChild(linkAgregarCarrito);
+    linkAgregarCarrito.appendChild(agregarCarrito);
+    });
 }
+
+
+/*! --------Product example--------
+
+<div class="col-lg-4 col-md-6 mb-4">
+    <div class="card shadow-sm h-100">
+        <a href="#!"><img class="shadow-sm card-img-top" src="https://dojiw2m9tvv09.cloudfront.net/11132/product/misterbig3308256.jpg"
+                alt="..." /></a>
+        <div class="card-body shadow-sm">
+            <h6 class="card-title text-center mt-3">Energetica MR BIG</h6>
+            <h5 class="text-center">$ 1.990</h5>
+        </div>
+        <div class="mx-auto my-2 ">
+            <a href="#!">
+                <img  src="/img/outline_add_shopping_cart_black_24dp.png" alt="">
+            </a>
+        </div>
+</div>
+*/
